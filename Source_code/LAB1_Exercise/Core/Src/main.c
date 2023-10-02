2b00a3758e997347884160e07e85e5a236a80d3a
Plunderer <lenhattien26.com>
2023-10-02 21:00:42 +0700

    Update Ex1

--- Source_code/LAB1_Exercise/Core/Src/main.c
+++ Source_code/LAB1_Exercise/Core/Src/main.c
@@ -95,6 +95,12 @@ int main(void)
   {
     /* USER CODE END WHILE */
 
+	  HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, SET);
+	  HAL_Delay(2000);
+	  HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, SET);
+	  HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, RESET);
+	  HAL_Delay(2000);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
